09d6b1d792afa6f67a5ed5ce5f036e7cac2ae2b5
Yayoi Matsuoka <y-matsuoka@quartetcom.co.jp>
2017-05-16 08:56:48 +0900

    サンプル問題を読み込む

--- src/app/repository/puzzle-repository.ts
+++ src/app/repository/puzzle-repository.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Cell, factory } from '../models/cell';
+import { problems } from '../problems';
 
 @Injectable()
 export class PuzzleRepository {
   get(): Cell[] {
-    function random(min: number, max: number): number {
-      return Math.floor(Math.random() * (max - min)) + min;
-    }
-
-    return Array(8).fill(0).map((n: number, i: number) => {
-      return factory(random(1, 9), random(1, 9), i + 1);
+    // TODO 問題の番号を指定できるように
+    return this.flatten(problems[0]).map((n: number, i: number) => {
+      return factory(Math.floor(i / 9), i % 9, n);
     });
+  }
+
+  private flatten(numbers: number[][]): number[] {
+    return [].concat.apply([], numbers);
   }
 }
